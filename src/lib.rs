669543e56b0293fb6c69b46719fc04ee5148a902
gabrielstoica <marius_gabriel1998@yahoo.com>
2024-11-02 20:52:18 +0200

    feat: add 'P256Verifier' impl

--- src/lib.rs
+++ src/lib.rs
@@ -1,68 +1,34 @@
-//!
-//! Stylus Hello World
-//!
-//! The following contract implements the Counter example from Foundry.
-//!
-//! ```
-//! contract Counter {
-//!     uint256 public number;
-//!     function setNumber(uint256 newNumber) public {
-//!         number = newNumber;
-//!     }
-//!     function increment() public {
-//!         number++;
-//!     }
-//! }
-//! ```
-//!
-//! The program is ABI-equivalent with Solidity, which means you can call it from both Solidity and Rust.
-//! To do this, run `cargo stylus export-abi`.
-//!
-//! Note: this code is a template-only and has not been audited.
-//!
-
 // Allow `cargo stylus export-abi` to generate a main function.
+#![allow(dead_code)]
+#![allow(unused_imports)]
 #![cfg_attr(not(feature = "export-abi"), no_main)]
 extern crate alloc;
 
+use p256;
 /// Import items from the SDK. The prelude contains common traits and macros.
-use stylus_sdk::{alloy_primitives::U256, prelude::*};
+use stylus_sdk::prelude::*;
 
 // Define some persistent storage using the Solidity ABI.
 // `Counter` will be the entrypoint.
 sol_storage! {
     #[entrypoint]
-    pub struct Counter {
-        uint256 number;
+    pub struct P256Verifier {
     }
 }
 
-/// Declare that `Counter` is a contract with the following external methods.
+/// Declare that `P256Verifier` is a contract with the following external methods.
 #[public]
-impl Counter {
-    /// Gets the number from storage.
-    pub fn number(&self) -> U256 {
-        self.number.get()
-    }
-
-    /// Sets a number in storage to a user-specified value.
-    pub fn set_number(&mut self, new_number: U256) {
-        self.number.set(new_number);
-    }
-
-    /// Sets a number in storage to a user-specified value.
-    pub fn mul_number(&mut self, new_number: U256) {
-        self.number.set(new_number * self.number.get());
-    }
-
-    /// Sets a number in storage to a user-specified value.
-    pub fn add_number(&mut self, new_number: U256) {
-        self.number.set(new_number + self.number.get());
-    }
-
-    /// Increments `number` and updates its value in storage.
-    pub fn increment(&mut self) {
-        let number = self.number.get();
-        self.set_number(number + U256::from(1));
+impl P256Verifier {
+    /// Verifies the provided signature
+    pub fn verify_p256_signature(
+        &self,
+        message: String,
+        pub_x: String,
+        pub_y: String,
+        r: String,
+        s: String,
+    ) -> bool {
+        let result = p256::verify_signature_secp256r1(&pub_x, &pub_y, &message, &r, &s);
+        result
     }
 }
